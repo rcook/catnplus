25f75aa53feac576d73185a0e65c7480c4bc28c4
Richard Cook <rcook@rcook.org>
2017-02-22 15:12:14 -0800

    Add colour

--- src/CatNPlus/App.hs
+++ src/CatNPlus/App.hs
@@ -11,10 +11,12 @@ Portability : portable
 module CatNPlus.App (runApp) where
 
 import           CatNPlus.KeyPress
+import           Control.Exception
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Either
+import qualified System.Console.ANSI as ANSI
 import           System.Directory
 import           System.Environment
 import           Text.Printf
@@ -22,16 +24,26 @@ import           Text.Printf
 pageLength :: Int
 pageLength = 5
 
+withSGR :: [ANSI.SGR] -> IO a -> IO a
+withSGR sgrs = bracket_
+    (ANSI.setSGR sgrs)
+    (ANSI.setSGR [ANSI.SetColor ANSI.Foreground ANSI.Vivid ANSI.White])
+
 runApp :: IO ()
 runApp = do
     paths <- getArgs
     void $ runEitherT $ forM_ paths $ \p -> do
         content <- liftIO $ do
             path <- canonicalizePath p
-            putStrLn ("File: " ++ path)
+            withSGR [ANSI.SetColor ANSI.Foreground ANSI.Dull ANSI.Green] $
+                putStrLn path
             readFile path
         runEitherT $ forM_ (zip [1..] (lines content)) $ \(n, line) -> do
-            liftIO $ putStrLn (printf "%6d  %s" n line)
+            liftIO $ do
+                withSGR [ANSI.SetColor ANSI.Foreground ANSI.Dull ANSI.Yellow] $
+                    putStr (printf "%6d" n)
+                putStrLn $ "  " ++ line
+
             when (n `mod` pageLength == 0) $ do
                 shouldContinue <- liftIO $ do
                     c <- waitForKeyPress ['Q', 'q', ' ']
